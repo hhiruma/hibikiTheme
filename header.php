03535dc032736c788553782b68ecd9ee4d0ea41b
hyohyogo@yahoo.co.jp <hyohyogo@yahoo.co.jp>
2017-11-18 21:43:03 +0900

    localized resources

--- header.php
+++ header.php
@@ -6,12 +6,15 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" >
+		<!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"-->
+		<!--link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" -->
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/font-awesome.min.css">
 
-		<script src='https://unpkg.com/vue'></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<!--script src='https://unpkg.com/vue'></script-->
+		<script src='<?php echo get_template_directory_uri();?>/js/vue.js'></script>
+		<!--script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.js"></script-->
+		<!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script-->
+		<script src='<?php echo get_template_directory_uri();?>/js/jquery-3.2.1min.js'></script>
 		<script src="<?php echo get_template_directory_uri();?>/js/externalFiles/jquery.bgswitcher.js"></script>
 		<?php wp_head(); ?>
 	</head>
